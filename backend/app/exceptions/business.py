661cf52544e7027880aa800b2bb1620f2d9c5c69
meng <gozhuimeng@outlook.com>
2026-03-23 17:28:42 +0800

    feat(be|auth): 添加异常类和异常捕获

--- backend/app/exceptions/business.py
+++ backend/app/exceptions/business.py
@@ -5,3 +5,25 @@
 # Description: 业务错误异常
 
 
+class BusinessError(Exception):
+    code: int = 400
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(self.message)
+
+
+class RegisterError(BusinessError):
+    code: int = 400
+
+
+class AuthError(BusinessError):
+    code: int = 401
+
+
+class NotFoundError(BusinessError):
+    code: int = 404
+
+
+class AlreadyExistsError(BusinessError):
+    code: int = 409
