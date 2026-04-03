189af3cd4c1cc5bff8741ae36747b4937739e69c
meng <gozhuimeng@outlook.com>
2026-04-03 12:52:47 +0800

    feat(fe): add SearchableSelect component for dropdown with search

--- frontend/components/ui/index.js
+++ frontend/components/ui/index.js
@@ -10,6 +10,7 @@ export { default as ConfirmDialog } from './ConfirmDialog';
 export { default as EmptyState } from './EmptyState';
 export { default as Input } from './Input';
 export { default as Modal } from './Modal';
+export { default as SearchableSelect } from './SearchableSelect';
 export { default as Select } from './Select';
 export { default as StatusBadge } from './StatusBadge';
 export { default as Table } from './Table';
